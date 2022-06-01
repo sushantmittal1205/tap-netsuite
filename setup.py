359c8cef46890ae968ad18431c72b7c16383d86e
sushantmittal1205 <70239698+sushantmittal1205@users.noreply.github.com>
2022-06-01 15:40:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     classifiers=['Programming Language :: Python :: 3 :: Only'],
     py_modules=['tap_netsuite'],
     install_requires=[
-        'netsuitesdk==2.7.4', # USING THE HOTGLUE VERSION
+        'netsuitesdk==2.7.0', # USING THE HOTGLUE VERSION
         'requests==2.21.0',
         'singer-python==5.3.1',
         'xmltodict==0.11.0',
